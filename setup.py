71e0fca838895f46fcd62c02a4b454dcd198192a
Haiwang  Yang <yangh13@biowulf.nih.gov>
2018-04-09 16:41:44 -0400

    m

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bigWigVis',
+      version='1.0',
+      description='bigWig visualization for orthologs',
+      url='http://github.com/haiwangyang/demultiplexer',
+      author='Haiwang Yang',
+      author_email='haiwangyang@gmail.com',
+      license='MIT',
+      packages=['bigWigVis'],
+      zip_safe=False)
